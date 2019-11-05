79e89243320ba098391f0c28e9a3033c72e95f8c
EduardoSantana <eduardoamparos@gmail.com>
2019-11-05 15:47:50 -0500

    adding references

--- MoviesDynamo/Startup.cs
+++ MoviesDynamo/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DynamoDb.Libs.DynamoDb;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,6 +33,22 @@ namespace MoviesDynamo
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            /* Begin - This Code was added because the video examples was not working. - Eduardo Santana 2019 - 10 - 14 */
+            var awsOptions = Configuration.GetAWSOptions();
+            awsOptions.Credentials = new Amazon.Runtime.BasicAWSCredentials(Configuration["AWS:AccessKey"], Configuration["AWS:SecretKey"]);
+            awsOptions.Region = RegionEndpoint.CACentral1;
+            services.AddDefaultAWSOptions(awsOptions);
+            /* End */
+
+        
+            services.AddAWSService<IAmazonDynamoDB>();
+
+            services.AddSingleton(typeof(ICreateTable<>),typeof(CreateTable<>));
+            services.AddSingleton<IPutItem, PutItem>();
+            services.AddSingleton<IGetItem, GetItem>();
+            //services.AddSingleton<IUpdateItem, UpdateItem>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
